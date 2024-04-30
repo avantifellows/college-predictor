b075e5fbad1f55a736b2dfe434bdb1551c8e700e
Raj Aryan Singh <rajlysm15@gmail.com>
2024-04-30 10:22:53 +0530

    Table header update

--- components/PredictedCollegeTables.js
+++ components/PredictedCollegeTables.js
@@ -4,6 +4,14 @@ const TableCell = ({ content }) => (
   </td>
 );
 
+const TableHeader = ({ label, width }) => (
+  <th
+    scope="col"
+    className={`px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider ${width}`}
+  >
+    {label}
+  </th>
+);
 const PredictedCollegesTable = ({ data = [] }) => {
   return (
     <div className="w-full overflow-x-auto">
@@ -17,51 +25,16 @@ const PredictedCollegesTable = ({ data = [] }) => {
         <p className="text-sm font-semibold text-gray-600">OS: Out of State</p>
       </div>
       <table className="min-w-full divide-y divide-gray-200 shadow-md rounded-lg overflow-hidden">
-        <thead className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-            >
-              Institute Rank
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-            >
-              State
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider w-1/4"
-            >
-              Institute
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider w-1/3"
-            >
-              Academic Program Name
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-            >
-              Opening Rank
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-            >
-              Closing Rank
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-            >
-              Quota
-            </th>
-          </tr>
+        <thead className="bg-gradient-to-r from-yellow-400 via-orange-400 to-red-500">
+        <tr>
+        <TableHeader label="Institute Rank" />
+        <TableHeader label="State" />
+        <TableHeader label="Institute" width="w-1/4" />
+        <TableHeader label="Academic Program Name" width="w-1/3" />
+        <TableHeader label="Opening Rank" />
+        <TableHeader label="Closing Rank" />
+        <TableHeader label="Quota" />
+      </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {data.map((item, index) => (
