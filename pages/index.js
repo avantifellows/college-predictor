f30c3b70f4f60c7942e8f4eeeccb6809431a95a9
Raj Aryan Singh <rajlysm15@gmail.com>
2024-04-30 10:30:53 +0530

    button color changed

--- pages/index.js
+++ pages/index.js
@@ -197,7 +197,7 @@ const HomePage = () => {
             )}
             <button
               type="button"
-              className="w-full py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+              className="w-full py-2 px-4 bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
               onClick={handleSubmit}
               disabled={isSubmitDisabled}
             >
